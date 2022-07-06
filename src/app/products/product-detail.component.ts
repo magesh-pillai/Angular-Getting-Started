8cf3a995d7b097dc49f9cfa4b262ca09db5bb55d
Magesh Pillai <magesh-pillai@hotmail.com>
2022-07-06 11:27:41 -0400

    Initial Commit

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: IProduct | undefined;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle += `: ${id}`;
+    this.product = 
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2021",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "assets/images/hammer.png"
+    };
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
+}
